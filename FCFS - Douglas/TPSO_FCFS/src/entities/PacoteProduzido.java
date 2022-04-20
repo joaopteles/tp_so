1eca22de35aae3330772b96240773de6e2322d26
dgsilveira <dgsilveira@sga.pucminas.br>
2022-04-20 01:15:12 -0300

    FCFS_Douglas_up1
    Adicionado o código FCFS

---
+++ FCFS - Douglas/TPSO_FCFS/src/entities/PacoteProduzido.java
@@ -0,0 +1,61 @@
+package entities;
+
+import org.jetbrains.annotations.NotNull;
+
+public class PacoteProduzido implements Comparable <PacoteProduzido>{
+    private int pacoteNumero;
+    private double momentoProduzidoSegundos;
+    private String cliente;
+    private int pedidoNumero;
+    private int prazo;
+
+
+    public PacoteProduzido(int pacoteNumero,
+                           double momentoProduzidoSegundos,
+                           String cliente,
+                           int pedidoNumero,
+                           int prazo) {
+        this.pacoteNumero = pacoteNumero;
+        this.momentoProduzidoSegundos = momentoProduzidoSegundos;
+        this.cliente = cliente;
+        this.pedidoNumero = pedidoNumero;
+        this.prazo = prazo;
+    }
+
+    public int getPacoteNumero() {
+        return pacoteNumero;
+    }
+
+    public double getMomentoProduzidoSegundos() {
+        return momentoProduzidoSegundos;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public int getPrazo() {
+        return prazo;
+    }
+
+    public int getPedidoNumero() {
+        return pedidoNumero;
+    }
+
+    @Override
+    public int compareTo(@NotNull PacoteProduzido o) {
+        if(this.momentoProduzidoSegundos < o.getMomentoProduzidoSegundos()){
+            return -1;
+        } else if(this.momentoProduzidoSegundos > o.getMomentoProduzidoSegundos()){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Pacote Num= " + pacoteNumero +
+                ", Momento= " + momentoProduzidoSegundos;
+    }
+}
