cadfbc8cadc54078a5cd185bef20e5e71ba40b20
dgsilveira <dgsilveira@sga.pucminas.br>
2022-05-13 22:06:55 -0300

    Classe Empacotar - método criarListaPedidos do arquivo txt
    Classe Empacotar - método criarListaPedidos do arquivo txt

--- Parte2/src/entities/Empacotar.java
+++ Parte2/src/entities/Empacotar.java
@@ -1,8 +1,37 @@
 package entities;
 
+import util.ArquivoLeitura;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Empacotar {
 
-    private static final String NOME_ARQUIVO = "./arq-teste2.txt";
+    private static final String NOME_ARQUIVO = "./arq-teste.txt";
+
+    List<Pedido> pedidos = new ArrayList<>();
+
+    public Empacotar() {
+        criarListaPedidos();
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    private void criarListaPedidos() {
+        ArquivoLeitura al = new ArquivoLeitura(NOME_ARQUIVO);
+
+        String s = al.lerLinha();
+        int quantidadePedidos = Integer.parseInt(s);
+        for (int i = 0; i < quantidadePedidos; i++) {
+            String[] dadosPedido = al.lerLinha().split(";");
+            pedidos.add(new Pedido(dadosPedido[0],
+                    Integer.parseInt(dadosPedido[1]),
+                    Integer.parseInt(dadosPedido[2]),
+                    Integer.parseInt(dadosPedido[3])));
+        }
+    }
 
 }
 
