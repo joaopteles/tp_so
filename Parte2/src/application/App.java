2509f7514f937aff0a5cde495fa90d21c5a3dffe
dgsilveira <dgsilveira@sga.pucminas.br>
2022-05-13 22:06:19 -0300

    Update App.java

--- Parte2/src/application/App.java
+++ Parte2/src/application/App.java
@@ -1,4 +1,16 @@
 package application;
 
+import entities.Empacotar;
+import entities.Pedido;
+
+import java.util.List;
+
 public class App {
+    public static void main(String[] args) {
+        Empacotar e = new Empacotar();
+
+        for (Pedido p: e.getPedidos()) {
+            System.out.println(p.toString());
+        }
+    }
 }
